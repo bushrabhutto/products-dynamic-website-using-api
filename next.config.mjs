0dfa1bd6c5f223aabdc7575e3ec931120a74c669
Bushra <106545621+bushrabhutto@users.noreply.github.com>
2025-04-05 18:53:37 +0500

    images errors solved

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['dummyjson.com'],
+    },
+  };
+  
+  export default nextConfig;
+  
